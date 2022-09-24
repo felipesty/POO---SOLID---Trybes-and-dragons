aa24aa188e1f7f07fa59a081860cd997de803705
felipesty <felipestylex08@gmail.com>
2022-09-24 19:09:03 -0300

    req 4: criando a classe ArcheType

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,31 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _special: number;
+  private _cost: number;
+
+  constructor(private _name: string) {
+    this._cost = 0;
+    this._special = 0;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
